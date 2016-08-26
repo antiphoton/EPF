f657168bf5a55b6041f8c652bafd3c07de555fb7
cbx <caoboxiao@gmail.com>
2016-08-26 16:11:53 -0400

    Server port can be specified by console args

--- server/main.js
+++ server/main.js
@@ -2,6 +2,8 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const argv = require('yargs').argv;
+const port = argv.port || 8080;
 app.set('view engine','pug');
 app.get(/\/$/, function (req, res, next) {
     res.render(path.resolve(__dirname,'../html'+req.path+'index.pug'), function(err, html) {
@@ -15,5 +17,5 @@ app.get(/\/$/, function (req, res, next) {
     });
 });
 app.use(express.static(path.resolve(__dirname,'../html')));
-app.listen(8080);
+app.listen(port);
 
